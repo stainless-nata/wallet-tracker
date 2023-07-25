89d920edf08f2e43d1d2c8cb87412e003f8080b2
Dev-Bastians <workingkeo@gmail.com>
2023-07-25 09:43:51 -0400

    fix: save function routes

--- utils/save.js
+++ utils/save.js
@@ -2,7 +2,7 @@ import fs from "fs";
 
 const save = (type, obj) => {
     let myJSON = JSON.stringify(obj);
-    fs.writeFile(`../config/${type}.json`, myJSON, (err) => {
+    fs.writeFile(`./config/${type}.json`, myJSON, (err) => {
       if (err) console.log(err);
       // console.log(`${type} Saved!`);
     });
